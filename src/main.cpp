c2da544b4177cd4abf3a38d7ee934e2d3be78730
dtalmood <dtalmood1@gmail.com>
2024-05-28 17:11:21 -0700

    Main Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+void printMenu();
 
 int main()
 {
-    cout << "Hello\nHello\nHello\nHello\nHello\nHello\nHello\nHello\n " << endl;
+    printMenu();
+    cout << "Type your question and once done press enter to search. " << endl;
+    cout << "Question: ";
+    string userQuestion;
+    cin >>  userQuestion;
+    cout << "FUCK YOU" << endl;
     return 0;
+}
+
+void printMenu()
+{
+    int titleWidth = 40;
+    int titleLength = 12; // Length of "STACK SURFER"
+    int leftPadding = (titleWidth - titleLength) / 2; // Calculate left padding
+
+    cout << setw(titleWidth) << setfill('=') << "" << endl;
+    cout << setw(leftPadding) << setfill(' ') << "" << "STACK SURFER" << setw(titleLength) << setfill(' ') << "" << endl;
+    cout << setw(titleWidth) << setfill('=') << "" << endl;
 }
